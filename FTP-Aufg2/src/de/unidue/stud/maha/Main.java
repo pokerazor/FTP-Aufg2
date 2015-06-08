3dc50fd49d6916d5ded12609bce7c1e4d6b8cca4
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2015-06-08 12:27:49 +0200

    Create package and classes

---
+++ FTP-Aufg2/src/de/unidue/stud/maha/Main.java
@@ -0,0 +1,34 @@
+package de.unidue.stud.maha;
+import SoFTlib.Node;
+
+/**
+ * 
+ */
+
+/**
+ * @author Hanno - Felix Wagner
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public class Maskierer extends Node {
+		
+	}
+	
+	public class Prozess extends Node {
+		
+	}
+	
+
+}
+
+
+
