32f848315ce08b986adfcf59d056cbfda1d17d89
pokerazor <hanno-felix.wagner@stud.uni-due.de>
2015-06-08 16:22:52 +0200

    fixed return values

--- FTP-Aufg2/src/de/unidue/stud/maha/Main.java
+++ FTP-Aufg2/src/de/unidue/stud/maha/Main.java
@@ -11,27 +11,36 @@ public class Main extends SoFT {
 	/** @param args */
 	public static void main(String[] args) {
 		Node[] nodes = { new Prozess("A"), new Prozess("B"), new Prozess("C"), new Maskierer("D"), new Maskierer("E"), new Maskierer("F") };
-		new Main().runSystem(nodes, "mï¿½-Protokoll", "Aufgabe 2", "Marc Gesthï¿½sen, Hanno - Felix Wagner");
+		new Main().runSystem(nodes, "m²-Protokoll", "Aufgabe 2", "Marc Gesthüsen, Hanno - Felix Wagner");
 	}
 
 	public int result(String input, String[] output) {
-
-		int resultVal = determineResult(number(words(input, 1, 1, 1)), number(words(input, 1, 2, 2)), number(words(input, 1, 3, 3)), number(words(output[3], 1, 1, 1)), number(words(output[4], 1, 1, 1)), number(words(output[5], 1, 1, 1)));
+		int resultVal = determineResult(number(words(input, 1, 1, 1)), number(words(input, 1, 2, 2)), number(words(input, 1, 3, 3)), sanitize(output[3]), sanitize(output[4]), sanitize(output[5]));
 		return resultVal;
+	}
+	
+	public static int sanitize(String inputVal){
+		return Double.valueOf(inputVal).intValue();
 	}
 
 	public int determineResult(int firstInput, int secondInput, int thirdInput, int firstValue, int secondValue, int thirdValue) {
 		max = Math.max(thirdInput, Math.max(firstInput, secondInput));
 		min = Math.min(thirdInput, Math.min(firstInput, secondInput));
+		
+		System.out.println("max"+max);
+		System.out.println("min"+min);
+		System.out.println("firstInput"+firstInput);
+		System.out.println("secondInput"+secondInput);
+		System.out.println("thirdInput"+thirdInput);
+		System.out.println("firstValue"+firstValue);
+		System.out.println("secondValue"+secondValue);
+		System.out.println("thirdValue"+thirdValue);
 
 		if (isValid(firstValue) && isValid(secondValue) && isValid(thirdValue)) {
 			if (firstValue == secondValue && firstValue == thirdValue) {
 				return 0; // wenn alle drei Rückgabewerte korrekt und übereinstimmend sind
 			} else {
-				return 1; // wenn andernfalls alle drei Rï¿½ckgabewerte korrekt,
-							// aber nicht ï¿½bereinstimmend sind (d.h. mindestens
-							// ein Wert unterscheidet sich von den beiden
-							// anderen),
+				return 1; // wenn andernfalls alle drei Rückgabewerte korrekt, aber nicht übereinstimmend sind (d.h. mindestens ein Wert unterscheidet sich von den beiden anderen),
 			}
 		} else {
 			if (isValid(firstValue) && isValid(secondValue)) {
@@ -77,7 +86,7 @@ public class Main extends SoFT {
 	}
 
 	public boolean isValid(int val) {
-		if (val > min && val < max) {
+		if (val >= min && val <= max) {
 			return true;
 		} else {
 			return false;
